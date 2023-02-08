c8ca9e8fe0d39f7e768dd30615c5c90815e96f72
vantam8300 <vantam8300@gmail.com>
2023-02-08 01:20:35 -0800

    adding icons to nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/fontawesome-free-brands';
 import React, {useState} from 'react';
 import "../styles.css"
 
@@ -6,6 +9,7 @@ export default function Navigation({ currentPage, handlePageChange }) {
  
     return (
         <>
+        
             <div className="navbarcon flex justify-between items-center px-20 py-6 bg-transparent z-40 text-white lg:bg-black lg:px-6  w-full">
                 <h1 className='text-4xl  font-bold '>Van Tam</h1>
 
@@ -16,15 +20,14 @@ export default function Navigation({ currentPage, handlePageChange }) {
                         <li className='bla'><a href="#projects"></a>Projects</li>
                     </ul>
                     <div className="icon text-lg flex gap-4  lg:gap-8 lg:my-10">
-                        <i className="fa-brands fa-twitter border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
-                        <i className="fa-brands fa-instagram  border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
-                        <i className="fa-brands fa-github  border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
+                        <i className="border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"><FontAwesomeIcon icon={ faLinkedin }/></i>
+                        <i className=" border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"><FontAwesomeIcon icon={ faGithub }/></i>
                     </div>
                     <div className="btn">
                         <button className='border-[1px] border-white border-solid py-2 px-4 hover:bg-white hover:text-black font-bold'>Let's Connect</button>
                     </div>
                 </nav>
-                <i onClick={e => setDurum(!durum)} className="fa-solid fa-bars hidden lg:block text-2xl cursor-pointer" ></i>
+                <i onClick={e => setDurum(!durum)} className="hidden lg:block text-2xl cursor-pointer" ><FontAwesomeIcon icon={ faBars }/></i>
             </div>
 
         </>
